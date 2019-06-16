d8c5155f6cd15f1126f29471dda18f60738f2e3c
Paul Davis <pjdavis@gmx.com>
2019-06-16 21:36:04 +0100

    Update jwt prefix to match client

--- server/btk2/settings.py
+++ server/btk2/settings.py
@@ -124,7 +124,7 @@ REST_FRAMEWORK = {
 }
 
 JWT_AUTH = {
-    'JWT_AUTH_HEADER_PREFIX': 'JWT',
+    'JWT_AUTH_HEADER_PREFIX': 'Bearer',
     'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=3600)
 }
 
